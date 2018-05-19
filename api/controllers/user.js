5b8218d0ffaf666c2b73527282f683f626327fcb
Rohan <rohan.seth497@gmail.com>
2018-05-19 18:11:59 +0530

    changes

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -38,14 +38,6 @@ exports.user_signup = (req, res, next) => {
             })
         }
     })
-    
-    // new Promise((resolve, reject) => {
-    //     connection
-    // })
-    // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    //     if (error) throw error;
-    //     console.log('The solution is: ', results[0].solution);
-    // });
 }
 
 exports.user_login = (req, res, next) => {
