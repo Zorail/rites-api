994e20575347575ee8d71635e60cdea899f3b4b7
Rohan <rohan.seth497@gmail.com>
2018-05-19 15:47:00 +0530

    added user controller

---
+++ api/controllers/user.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.user_signup = (req, res, next) => {
+
+}
+
+exports.user_login = (req, res, next) => {
+
+}
+
+exports.user_delete = (req, res, next) => {
+    
+}
