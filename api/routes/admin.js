da6ca67da84ddacea06069009d5ce35c5c199ad0
Rohan <rohan.seth497@gmail.com>
2018-05-20 02:05:37 +0530

    all file types for admin

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -13,21 +13,21 @@ const storage = multer.diskStorage({
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    // reject a file
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
+// const fileFilter = (req, file, cb) => {
+//     // reject a file
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//       cb(null, true);
+//     } else {
+//       cb(null, false);
+//     }
+//   };
 
 const upload = multer({
     storage: storage,
     limits: {
       fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
+    }
+    // fileFilter: fileFilter
 });
 
 router.get('/', AdminController.get_vacancies);
