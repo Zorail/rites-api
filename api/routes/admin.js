31826a2e750509759f7aa73d305827616528df56
Rohan <rohan.seth497@gmail.com>
2018-05-20 02:04:53 +0530

    multer for admin

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 const AdminController = require('../controllers/admin');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads/admin');
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 router.get('/', AdminController.get_vacancies);
 
 router.get('/all', AdminController.get_vacancies_with_user);
 
-router.post('/', AdminController.vacancy_upload);
+router.post('/', upload.any(), AdminController.vacancy_upload);
 
 module.exports = router;
